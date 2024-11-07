f4361a8b51076e26df2bb0bf5caee0312afbed92
Cristian <cristiangoncas@gmail.com>
2024-11-06 19:30:29 -0500

    Open/Close principle Created a  to provide the logic for route and arguments that can be shared for every navigation element

--- app/src/main/java/com/group3/architectcoders/ui/screens/navigation/Navigation.kt
+++ app/src/main/java/com/group3/architectcoders/ui/screens/navigation/Navigation.kt
@@ -1,10 +1,9 @@
-package com.group3.architectcoders.ui.screens
+package com.group3.architectcoders.ui.screens.navigation
 
-import android.os.BaseBundle
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavArgumentBuilder
-import androidx.navigation.NavController
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,33 +17,58 @@ fun Navigation() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
+        composable(NavItem.Home) {
             HomeScreen(
-                onBookClick = { book -> navController.navigate("detail/${book.id}") },
-                onCamClick = { navController.navigate("camera") },
+                onBookClick = { book -> navController.navigate(NavItem.Detail.createRoute(book.id)) },
+                onCamClick = { navController.navigate(NavItem.Camera.route) },
                 onBookmarked = { book -> /* TODO: Save book */ },
             )
         }
-        composable(
-            route = "detail/{bookId}",
-            arguments = listOf(navArgument("bookId") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val bookId = requireNotNull(backStackEntry.arguments?.getString("bookId"))
+        composable(NavItem.Detail) { backStackEntry ->
             DetailScreen(
                 viewModel = viewModel {
                     com.group3.architectcoders.ui.screens.detail.DetailViewModel(
-                        bookId
+                        backStackEntry.findArg(
+                            NavArg.BookId
+                        )
                     )
                 },
                 onBack = { navController.popBackStack() },
                 onBookmarked = { book -> /* TODO: Save book */ },
             )
         }
-        composable("camera") {
+        composable(NavItem.Camera) {
             com.group3.architectcoders.ui.screens.camera.CameraScreen(
                 onBack = { navController.popBackStack() },
                 onBookClick = { book -> navController.navigate("detail/${book.id}") },
             )
         }
     }
+}
+
+private fun NavGraphBuilder.composable(
+    navItem: NavItem,
+    content: @Composable (NavBackStackEntry) -> Unit
+) {
+    composable(
+        route = navItem.route,
+        arguments = navItem.args
+    ) {
+        content(it)
+    }
+}
+
+private inline fun <reified T> NavBackStackEntry.findArg(arg: NavArg): T {
+    requireNotNull(arguments)
+
+    val value: Any? = when (T::class) {
+        String::class -> arguments?.getString(arg.key)
+        Int::class -> arguments?.getInt(arg.key)
+        Boolean::class -> arguments?.getBoolean(arg.key)
+        Float::class -> arguments?.getFloat(arg.key)
+        Long::class -> arguments?.getLong(arg.key)
+        else -> null
+    }
+    requireNotNull(value)
+    return value as T
 }
